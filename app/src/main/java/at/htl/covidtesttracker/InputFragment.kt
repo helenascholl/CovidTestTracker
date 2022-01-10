7206039793aa86069561408bbe29ac1c8c50d325
schollsebastian <scholl.sebastian@gmx.at>
2022-01-10 14:52:37 +0100

    Send Test to ListFragment

--- app/src/main/java/at/htl/covidtesttracker/InputFragment.kt
+++ app/src/main/java/at/htl/covidtesttracker/InputFragment.kt
@@ -6,13 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import at.htl.covidtesttracker.databinding.FragmentInputBinding
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class InputFragment : Fragment() {
 
     private lateinit var binding: FragmentInputBinding
-    private var locations = arrayOf("Linz-Stadtplatz",
-        "Leonding-Meixnerkreuzung", "Parkplatz-Haidcenter")
+    private var locations = arrayOf(
+        "Linz-Stadtplatz", "Leonding-Meixnerkreuzung", "Parkplatz-Haidcenter"
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,15 +24,35 @@ class InputFragment : Fragment() {
     ): View {
         binding = FragmentInputBinding.inflate(inflater, container, false)
 
-        val resultAdapter = ArrayAdapter(requireContext(),
-            android.R.layout.simple_spinner_item, TestResult.values())
+        val resultAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_item, TestResult.values()
+        )
         resultAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.editResult.adapter = resultAdapter
 
-        val locationAdapter = ArrayAdapter(requireContext(),
-            android.R.layout.simple_spinner_item, locations)
+        val locationAdapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_item, locations
+        )
         locationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.editLocation.adapter = locationAdapter
+
+        binding.save.setOnClickListener { view ->
+            val result = Test(
+                binding.id.text.toString(),
+                LocalDateTime.parse(
+                    "${binding.editDate.text}T${binding.editTime.text}",
+                    DateTimeFormatter.ofPattern("dd/MM/yyyy'T'HH:mm")
+                ),
+                TestResult.valueOf(binding.editResult.selectedItem.toString()),
+                binding.editLocation.selectedItem.toString()
+            )
+            val action = InputFragmentDirections.actionInputFragmentToListFragment(result)
+            view
+                .findNavController()
+                .navigate(action)
+        }
 
         return binding.root
     }
