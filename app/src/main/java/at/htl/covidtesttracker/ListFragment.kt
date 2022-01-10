e4759495e98d8e5957dc662b4a5e8d67699b8872
schollsebastian <scholl.sebastian@gmx.at>
2022-01-10 16:38:02 +0100

    Make ListFragment the home fragment

--- app/src/main/java/at/htl/covidtesttracker/ListFragment.kt
+++ app/src/main/java/at/htl/covidtesttracker/ListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import at.htl.covidtesttracker.databinding.FragmentListBinding
 import java.time.format.DateTimeFormatter
@@ -22,13 +23,21 @@ class ListFragment : Fragment() {
     ): View {
         binding = FragmentListBinding.inflate(inflater, container, false)
 
-        items.add(args.result)
+        args.result?.let {
+            items.add(it)
+        }
 
         val adapter = ArrayAdapter(
             requireContext(),
             android.R.layout.simple_list_item_1, items.map(this::toListString)
         )
         binding.list.adapter = adapter
+
+        binding.addTest.setOnClickListener { view ->
+            view
+                .findNavController()
+                .navigate(R.id.action_listFragment_to_inputFragment)
+        }
 
         return binding.root
     }
