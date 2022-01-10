d42bcaebdc8f2d3b521440c3822f556bba9c3685
schollsebastian <scholl.sebastian@gmx.at>
2022-01-10 13:15:58 +0100

    Populate Spinners

--- app/src/main/java/at/htl/covidtesttracker/InputFragment.kt
+++ app/src/main/java/at/htl/covidtesttracker/InputFragment.kt
@@ -1,59 +1,36 @@
 package at.htl.covidtesttracker
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import at.htl.covidtesttracker.databinding.FragmentInputBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [InputFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class InputFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentInputBinding
+    private var locations = arrayOf("Linz-Stadtplatz",
+        "Leonding-Meixnerkreuzung", "Parkplatz-Haidcenter")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_input, container, false)
+    ): View {
+        binding = FragmentInputBinding.inflate(inflater, container, false)
+
+        val resultAdapter = ArrayAdapter(requireContext(),
+            android.R.layout.simple_spinner_item, TestResult.values())
+        resultAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.editResult.adapter = resultAdapter
+
+        val locationAdapter = ArrayAdapter(requireContext(),
+            android.R.layout.simple_spinner_item, locations)
+        locationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.editLocation.adapter = locationAdapter
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InputFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InputFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
